f54d3d2ebc63618a8feffc4526f76fab10c8464f
Matthew Thornett <matt.thornett1998@gmail.com>
2025-01-23 23:16:01 -0700

    Pasted content of context.js

---
+++ trpc/context.ts
@@ -0,0 +1,14 @@
+// trpc/context.ts
+import { createTRPCContext } from "@trpc/server";
+import { type inferAsyncReturnType } from "@trpc/server";
+
+export const createContext = async () => {
+  const session = await getSession();
+  const ctx = {
+    session,
+  }
+
+  return ctx;
+};
+
+export type Context = typeof createContext;
