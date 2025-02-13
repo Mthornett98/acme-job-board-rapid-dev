5213f52957099321bb0283a42d09be14674387b5
Matthew Thornett <matt.thornett1998@gmail.com>
2025-02-13 09:40:10 +0800

    Updated trpc.ts to include a public, private, and admin procedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -2,13 +2,31 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import { Context } from "./context";
 
-const t = initTRPC.context<Context>().create({
-  transformer: superjson,
+const t = initTRPC.context<Context>().create();
+
+//Middleware for authentication
+const isAuth = t.middleware(({ ctx, next }) => {
+  if (!ctx.userID) {
+    throw new TRPCError({ code: 'UNAUTHORIZED' });
+  }
+  return next({ ctx });
 });
 
-export const middleware = t.middleware;
-export const createCallerFactory = t.createCallerFactory;
-export const mergeRouters = t.mergeRouters;
+const isOptionalAuth = t.middleware(({ ctx, next }) => {
+  return next({ ctx });
+});
+
+const isAdmin = t.middleware(({ ctx, next}) => {
+  if (ctx.role !== 'ADMIN') {
+    throw new TRPCError ({ code: 'FORBIDDEN'});
+  }
+  return next ({ ctx });
+});
+
+//Procedure builders
+export const publicProcedure = t.procedure.use(isOptionalAuth);
+export const privateProcedure = t.procedure.use(isAuth);
+export const adminProcedure = t.procedure.use(isAuth).use(isAdmin);
 
 export const router = t.router;
-export const procedure = t.procedure;
+export const middleware = t.middleware; 
