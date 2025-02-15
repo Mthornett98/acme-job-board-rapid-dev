040296d82b089522efdfd14c226f25cad91dfe4a
Matthew Thornett <matt.thornett1998@gmail.com>
2025-02-15 13:44:13 +0800

    Defined Type for Context

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,16 +1,10 @@
 import { auth } from "@/app/auth"; //Bringing in function called auth from /app/auth
 import { type Session } from "@next-auth"; //Imports a session from next-auth, a library used for auth in Next.js
 
-const handler = (request: Request) => {
-  return fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req: request,
-    router: appRouter,
-    createContext: createContext,
-    onError({ error }) {
-      console.error('tRPC error:', error);
-    },
-  });
-};
+export type Context = { //Creates a custom type called Context - essentially an object schema
+  session: Session | null; //States that session can either be a Session object (if someone is logged in) or null if no one is logged in
+  userID?: string; //States that userID is a string and is optional (?)
+  role?: string; //States that the role is a string and is an optional property (due to ?)
+} 
 
 export { handler as GET, handler as POST };
